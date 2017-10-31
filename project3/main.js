0f920839c4e88bbba4306118e4fab952c1fbb439
selenaqian <31712909+selenaqian@users.noreply.github.com>
2017-10-31 00:40:59 -0400

    task 2

--- project3/main.js
+++ project3/main.js
@@ -1,9 +1,16 @@
-var p1;
-p1 = 0;
+var p1 = 0;
 function f1(x) {
-    var output = 5*x - 3*x*x +13;
-    return output;
+    var output1 = 5*x - 3*x*x +13;
+    return output1;
 }
 p1 = f1(3);
 var el = document.getElementById('p1');
 el.textContent = 'f(x) = ' + p1;
+var p2 = 0;
+function f2(x, y, z) {
+    var output2 = x + " " + y + " " + z;
+    return output2;
+}
+p2 = f2('John', 'Jacob', 'Smith');
+var el = document.getElementById('p2');
+el.textContent = 'Hello, ' + p2 + '!';
