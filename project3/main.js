98fafbe77c6e2ad9e201acb4bd104f18f0a70570
selenaqian <31712909+selenaqian@users.noreply.github.com>
2017-11-01 13:06:59 -0400

    edits to take in inputs

--- project3/main.js
+++ project3/main.js
@@ -1,61 +1,55 @@
-var p1 = 0;
-function f1(x) {
-    var output1 = 5*x - 3*x*x +13;
-    return output1;
+function f1() {
+    var number = parseFloat(document.getElementById("num1").value);
+    var p1 = 5*number - 3*number*number +13;
+    var el = document.getElementById('p1').innerHTML = 'f(' + number.toString() + ') = ' + p1.toString();
 }
-p1 = f1(3);
-var el = document.getElementById('p1');
-el.textContent = 'f(x) = ' + p1;
 
-var p2 = 0;
-function f2(x, y, z) {
-    var output2 = x + " " + y + " " + z;
-    return output2;
+function f2() {
+    var first = document.getElementById("name1");
+    var middle = document.getElementById("name2");
+    var last = document.getElementById("name3");
+    var p2 = first + " " + middle + " " + last;
+    var el = document.getElementById('p2').innerHTML = "Hello, " + p2 + "!";
 }
-p2 = f2('John', 'Jacob', 'Smith');
-var el = document.getElementById('p2');
-el.textContent = 'Hello, ' + p2 + '!';
 
-var p3 = '';
-var color1 = '';
-function f3(x, y) {
+function f3() {
+    var p3 = '', color1 = '';
+    var x = parseFloat(document.getElementById("numx").value);
+    var y = parseFloat(document.getElementById("numy").value);
     if (x > y) {
         color1 = 'yellow';
-        return 'bananas';
+        p3 = 'bananas';
     } else if (x < y) {
         color1 = 'brown';
-        return 'porcupine';
+        p3 = 'porcupine';
     } else if (x === y && x < 0) {
         color1 = 'red';
-        return 'red apples';
+        p3 = 'red apples';
     } else {
         color1 = 'red';
-        return 'green apples';
-    }
+        p3 = 'green apples';
+    }   document.getElementById('p3').innerHTML = p3;
+    document.getElementById('p3').style.color = color1;
 }
-p3 = f3(3, 2);
-var el = document.getElementById('p3');
-el.textContent = p3;
 
-var p4 = '';
-var color2 = '';
 function f4(x) {
-    x = x.replace(/\D/g,'');
+    var p4 = '', color2 = '', output4 = '';
+    p4 = document.getElementById("phonenum").value;
+    p4 = p4.replace(/\D/g,'');
     var firstchar = p4.substring(0,1);
-    if (x.length === 10) {
+    if (p4.length === 10) {
         color2 = 'green';
-        return 'valid';
-    } else if (x.length === 11 && firstchar === "1") {
+        output4 = 'valid';
+    } else if (p4.length === 11 && firstchar === "1") {
         color2 = 'green';
-        return 'valid';
+        output4 = 'valid';
     } else {
         color2 = 'red';
-        return 'invalid';
+        output4 'invalid';
     }
+    document.getElementById('p4').innerHTML = output4;
+    document.getElementById('p4').style.color = color2;
 }
-p4 = f4('(123)-456-7890');
-var el = document.getElementById('p4');
-el.textContent = p4;
 
 var p5 = '';
 function f5(x) {
@@ -65,3 +59,7 @@ function f5(x) {
 p5 = f5(['apples', 'bananas', 'oranges', 'pears', 'peaches', 'pineapple']);
 var el = document.getElementById('p5');
 el.textContent = p5;
+
+function colorchange(event) {
+    
+}
