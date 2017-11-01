b7fcb7e07409719fc0e133a9b0f6e62480660cc3
selenaqian <31712909+selenaqian@users.noreply.github.com>
2017-10-31 23:36:23 -0400

    task 3

--- project3/main.js
+++ project3/main.js
@@ -6,6 +6,7 @@ function f1(x) {
 p1 = f1(3);
 var el = document.getElementById('p1');
 el.textContent = 'f(x) = ' + p1;
+
 var p2 = 0;
 function f2(x, y, z) {
     var output2 = x + " " + y + " " + z;
@@ -14,3 +15,24 @@ function f2(x, y, z) {
 p2 = f2('John', 'Jacob', 'Smith');
 var el = document.getElementById('p2');
 el.textContent = 'Hello, ' + p2 + '!';
+
+var p3 = '';
+var thiscolor = '';
+function f3(x, y) {
+    if (x > y) {
+        var thiscolor = 'yellow';
+        return 'bananas';
+    } else if (x < y) {
+        var thiscolor = 'brown';
+        return 'porcupine';
+    } else if (x === y && x < 0) {
+        var thiscolor = 'red';
+        return 'red apples';
+    } else {
+        var thiscolor = 'red';
+        return 'green apples';
+    }
+}
+p3 = f3(3, 2);
+var el = document.getElementById('p3');
+el.textContent = p3;
