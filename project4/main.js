111f464a012a4e695ce87a34fbe2634d45e1050c
selenaqian <31712909+selenaqian@users.noreply.github.com>
2017-11-21 13:03:17 -0500

    project4 task2, reset buttons

--- project4/main.js
+++ project4/main.js
@@ -8,6 +8,14 @@ $(function () {
         var $num1 = parseFloat($('#num1').val());
         var $ans1 = 5*$num1 - 3*$num1*$num1 + 13;
         $('#p1').append($ans1);
-        console.log($ans1);
+    });
+});
+$(function () {
+    $('#b2').on('click', function(e) {
+        var $name1 = $('#name1').val();
+        var $name2 = $('#name2').val();
+        var $name3 = $('#name3').val();
+        var $ans2 = 'Hello, ' + $name1 + ' ' + $name2 + ' ' + $name3 + '!';
+        $('#p2').append($ans2);
     });
 });
