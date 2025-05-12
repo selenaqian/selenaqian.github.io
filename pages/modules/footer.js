c624c57f9d35b3091f7addb091d91346a17907b1
Selena Qian <selenaq2010@gmail.com>
2025-05-12 12:22:54 -0400

    make footer common

---
+++ pages/modules/footer.js
@@ -0,0 +1,26 @@
+class CustomFooter extends HTMLElement {
+    constructor(){
+        super();
+
+        this.innerHTML = `
+        <footer>
+            <div>
+                <h3>Find My Work</h3>
+                <div id="my-work-links">
+                    <a href="https://www.linkedin.com/in/selena-qian" target="_blank" rel="noopener noreferrer">
+                        <img src="./assets/linkedin.svg"></img>
+                    </a>
+                    <a href="https://github.com/selenaqian" target="_blank" rel="noopener noreferrer">
+                        <img src="./assets/github-mark.svg"></img>
+                    </a>
+                    <a href="https://www.instagram.com/selenaq.art/" target="_blank" rel="noopener noreferrer">
+                        <img src="./assets/Instagram_Glyph_Black.svg"></img>
+                    </a>
+                </div>
+            </div>
+            <p>Copyright &copy; 2025 Selena Qian</p>
+        </footer>`;
+    }
+}
+
+window.customElements.define('custom-footer', CustomFooter);
