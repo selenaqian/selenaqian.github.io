616b6f1f09c8331dff291dabc5976dbea8427d9c
Selena Qian <selenaq2010@gmail.com>
2025-05-12 13:50:37 -0400

    footer sticks to bottom of page and end of content

--- pages/modules/footer.js
+++ pages/modules/footer.js
@@ -4,19 +4,17 @@ class CustomFooter extends HTMLElement {
 
         this.innerHTML = `
         <footer>
-            <div>
-                <h3>Find My Work</h3>
-                <div id="my-work-links">
-                    <a href="https://www.linkedin.com/in/selena-qian" target="_blank" rel="noopener noreferrer">
-                        <img src="./assets/linkedin.svg"></img>
-                    </a>
-                    <a href="https://github.com/selenaqian" target="_blank" rel="noopener noreferrer">
-                        <img src="./assets/github-mark.svg"></img>
-                    </a>
-                    <a href="https://www.instagram.com/selenaq.art/" target="_blank" rel="noopener noreferrer">
-                        <img src="./assets/Instagram_Glyph_Black.svg"></img>
-                    </a>
-                </div>
+            <h3>Find My Work</h3>
+            <div id="my-work-links">
+                <a href="https://www.linkedin.com/in/selena-qian" target="_blank" rel="noopener noreferrer">
+                    <img src="./assets/linkedin.svg"></img>
+                </a>
+                <a href="https://github.com/selenaqian" target="_blank" rel="noopener noreferrer">
+                    <img src="./assets/github-mark.svg"></img>
+                </a>
+                <a href="https://www.instagram.com/selenaq.art/" target="_blank" rel="noopener noreferrer">
+                    <img src="./assets/Instagram_Glyph_Black.svg"></img>
+                </a>
             </div>
             <p>Copyright &copy; 2025 Selena Qian</p>
         </footer>`;
