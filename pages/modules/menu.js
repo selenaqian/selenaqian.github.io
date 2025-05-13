745503213397997c572458a356c9a6f96b4ca5fc
Selena Qian <selenaq2010@gmail.com>
2025-05-13 10:35:11 -0400

    redo menu in js to be able to more easily access the elements

--- pages/modules/menu.js
+++ pages/modules/menu.js
@@ -1,29 +1,58 @@
+const PAGES = [
+    {title: 'Code', path: 'code.html'},
+    {title: 'Design', path: 'design.html'},
+    {title: 'Art', path: 'art.html'},
+    {title: 'Writing', path: 'writing.html'},
+];
+
 class MainMenu extends HTMLElement {
     constructor(){
         super();
 
+        const main = document.createElement('nav');
+        main.id = 'main-menu';
+        main.classList.add('navigation');
+        const list = document.createElement('ul');
+        list.classList.add('container');
+        let pathPrefix;
+        const home = document.createElement('li');
+        const homeLink = document.createElement('a');
+
         if (window.location.href.includes('pages')) {
-            this.innerHTML = `
-                <nav id='main-menu'><ul class='container'>
-                    <li class='child'><a href='../index.html'>Home</a></li>
-                    <li class='child'><a href='./code.html'>Code</a></li>
-                    <li class='child'><a href='./design.html'>Design</a></li>
-                    <li class='child'><a href='./art.html'>Art</a></li>
-                    <li class='child'><a href='./writing.html'>Writing</a></li>
-                </ul></nav>
-            `; 
+            pathPrefix = './';
+            homeLink.href = '../index.html';
         }
         else {
-            this.innerHTML = `
-                <nav id='main-menu'><ul class='container'>
-                    <li class='child'><a href='./index.html'>Home</a></li>
-                    <li class='child'><a href='./pages/code.html'>Code</a></li>
-                    <li class='child'><a href='./pages/design.html'>Design</a></li>
-                    <li class='child'><a href='./pages/art.html'>Art</a></li>
-                    <li class='child'><a href='./pages/writing.html'>Writing</a></li>
-                </ul></nav>
-            `;
+            pathPrefix = './pages/';
+            homeLink.href = './index.html';
         }
+
+        if (window.location.href.includes('index.html')) {
+            homeLink.classList.add('current');
+        }
+        homeLink.innerText = 'Home';
+        home.classList.add('child');
+        home.appendChild(homeLink);
+        list.appendChild(home);
+
+
+        for (let i = 0; i < PAGES.length; i++) {
+            const navPage = document.createElement('li');
+            navPage.classList.add('child');
+
+            const link = document.createElement('a');
+            link.href = `${pathPrefix}${PAGES[i].path}`;
+            link.innerText = PAGES[i].title;
+            if (window.location.href.includes(PAGES[i].path)) {
+                link.classList.add('current');
+            }
+
+            navPage.appendChild(link);
+            list.appendChild(navPage);
+        }
+
+        main.appendChild(list);
+        this.appendChild(main);
     }
 }
 
