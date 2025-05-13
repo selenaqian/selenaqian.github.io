d0955ade714a8823681b0e2e83d0183562c9da81
Selena Qian <selenaq2010@gmail.com>
2025-05-13 11:21:51 -0400

    navigation menu collapses at smaller sizes

--- pages/modules/menu.js
+++ pages/modules/menu.js
@@ -13,8 +13,10 @@ class MainMenu extends HTMLElement {
         main.id = 'main-menu';
         main.classList.add('navigation');
         main.classList.add('container');
-        let pathPrefix;
+        main.classList.add('closed');
+        this.appendChild(main);
 
+        let pathPrefix;
         const home = document.createElement('a');
 
         if (window.location.href.includes('pages')) {
@@ -46,7 +48,32 @@ class MainMenu extends HTMLElement {
             main.appendChild(link);
         }
 
-        this.appendChild(main);
+        // add menu link for dropdown
+        const dropdown = document.createElement('a');
+        dropdown.href = 'javascript:void(0);';
+        dropdown.classList.add('dropdown');
+        dropdown.innerText = 'Menu';
+        dropdown.addEventListener('click', (e) => {
+            responsiveDropdown();
+            if (e.target.innerText.includes('Menu')) {
+                e.target.innerText = "Close";
+            } else {
+                e.target.innerText = "Menu";
+            }
+        });
+        main.appendChild(dropdown);
+    }
+}
+
+
+function responsiveDropdown() {
+    let menu = document.getElementById("main-menu");
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        menu.classList.add('closed');
+    } else {
+        menu.classList.remove('closed');
+        menu.classList.add('open');
     }
 }
 
