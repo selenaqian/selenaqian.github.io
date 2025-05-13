3005cd04aabeb3d001cb6e9758eaaf3f5092833b
Selena Qian <selenaq2010@gmail.com>
2025-05-13 11:26:48 -0400

    add footer and 'main' class formatting to all pages

--- pages/modules/footer.js
+++ pages/modules/footer.js
@@ -2,18 +2,27 @@ class CustomFooter extends HTMLElement {
     constructor(){
         super();
 
+        let pathPrefix;
+
+        if (window.location.href.includes('pages')) {
+            pathPrefix = '../assets/';
+        }
+        else {
+            pathPrefix = './assets/';
+        }
+
         this.innerHTML = `
         <footer>
             <h3>Find My Work</h3>
             <div id="my-work-links">
                 <a href="https://www.linkedin.com/in/selena-qian" target="_blank" rel="noopener noreferrer">
-                    <img src="./assets/linkedin.svg"></img>
+                    <img src="${pathPrefix}linkedin.svg"></img>
                 </a>
                 <a href="https://github.com/selenaqian" target="_blank" rel="noopener noreferrer">
-                    <img src="./assets/github-mark.svg"></img>
+                    <img src="${pathPrefix}github-mark.svg"></img>
                 </a>
                 <a href="https://www.instagram.com/selenaq.art/" target="_blank" rel="noopener noreferrer">
-                    <img src="./assets/Instagram_Glyph_Black.svg"></img>
+                    <img src="${pathPrefix}Instagram_Glyph_Black.svg"></img>
                 </a>
             </div>
             <p>Copyright &copy; 2025 Selena Qian</p>
