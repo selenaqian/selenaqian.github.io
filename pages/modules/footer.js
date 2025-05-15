2fece1011e8973fc141ea8f61e33bcaad8007083
Selena Qian <selenaq2010@gmail.com>
2025-05-15 12:38:50 -0400

    some asset reorganizing, add design page

--- pages/modules/footer.js
+++ pages/modules/footer.js
@@ -16,13 +16,13 @@ class CustomFooter extends HTMLElement {
             <h3>Find My Work</h3>
             <div id="my-work-links">
                 <a href="https://www.linkedin.com/in/selena-qian" target="_blank" rel="noopener noreferrer">
-                    <img src="${pathPrefix}linkedin.svg"></img>
+                    <img src="${pathPrefix}linkedin.svg">
                 </a>
                 <a href="https://github.com/selenaqian" target="_blank" rel="noopener noreferrer">
-                    <img src="${pathPrefix}github-mark.svg"></img>
+                    <img src="${pathPrefix}github-mark.svg">
                 </a>
                 <a href="https://www.instagram.com/selenaq.art/" target="_blank" rel="noopener noreferrer">
-                    <img src="${pathPrefix}Instagram_Glyph_Black.svg"></img>
+                    <img src="${pathPrefix}Instagram_Glyph_Black.svg">
                 </a>
             </div>
             <p>Copyright &copy; 2025 Selena Qian</p>
